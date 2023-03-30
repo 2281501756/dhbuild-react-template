a3d5ab01060f3e25dfdc1d32a3352d691850974d
benzhi_macmini <macmini@benzhi.biz>
2023-03-30 18:08:41 +0800

    new file:   .editorconfig   new file:   .eslintignore 	new file:   .eslintrc.js 	new file:   .husky/pre-commit 	new file:   .prettierignore 	new file:   .prettierrc.js 	new file:   .stylelintignore 	new file:   .stylelintrc.js 	modified:   package.json 	modified:   pnpm-lock.yaml 	modified:   src/App.tsx 	modified:   src/index.module.less 	modified:   tsconfig.json

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
 import styles from './index.module.less'
 
 const App = () => {
-  console.log(process.env)
   return (
     <div>
       <div className={styles.box}>
         <span>123</span>
       </div>
-      <div className="box"></div>
+      <div className='box'></div>
       123 123 123
     </div>
   )
